59bed73623842ef1e68d30e66c642e2b844f959a
Shaked Livyani <shakedliv38@gmail.com>
2025-10-28 15:00:54 +0200

    Limit ball size to 400

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function onBallClick(elBall) {
-   var ballSize = +elBall.innerText
-   ballSize += 50
-   elBall.style.width = ballSize + 'px'
-   elBall.style.height = ballSize + 'px'
-   elBall.innerText = ballSize
+    var ballSize = +elBall.innerText
+    if (ballSize < 400) ballSize += 50
+   else ballSize = 100
+   
+    elBall.style.width = ballSize + 'px'
+    elBall.style.height = ballSize + 'px'
+    elBall.innerText = ballSize
 }
