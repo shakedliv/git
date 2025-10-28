d4fbf2e171073e2f00ef1e4df729418fa5f7897f
Shaked Livyani <shakedliv38@gmail.com>
2025-10-28 15:23:17 +0200

    Randomize ball color upon click

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function onBallClick(elBall) {
+
     var ballSize = +elBall.innerText
     ballSize += getRandomInt(20, 60)
-    if (ballSize > 400) ballSize = 100
-   
+   if (ballSize > 400) ballSize = 100
+    const rndColor = getRandomColor()
+    elBall.style.backgroundColor = rndColor
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerText = ballSize
