d09f585e4d82fd412be4a8aea33f8e42577a8bd2
Shaked Livyani <shakedliv38@gmail.com>
2025-10-28 14:40:41 +0200

    Fix onBallClick function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function onBallClick() {
-   console.log('clicked the div');
+function onBallClick(elBall) {
+   var ballSize = +elBall.innerText
+   ballSize += 50
+   elBall.style.width = ballSize + 'px'
+   elBall.style.height = ballSize + 'px'
+   elBall.innerText = ballSize
 }
